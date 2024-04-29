0d58f2bbe33315ab2980c124416f7d84f85bdc98
Istam Rakhimov <vupmud@gmail.com>
2024-04-29 12:41:33 +0300

    added inferencer with LoadDicomImage support

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .det_inferencer import DetInferencer
+from .med_inferencer import MedInferencer
 from .inference import (async_inference_detector, inference_detector,
                         inference_mot, init_detector, init_track_model)
 
 __all__ = [
     'init_detector', 'async_inference_detector', 'inference_detector',
-    'DetInferencer', 'inference_mot', 'init_track_model'
+    'DetInferencer', 'MedInferencer', 'inference_mot', 'init_track_model'
 ]
