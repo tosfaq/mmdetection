de49a7c7c28b38074b74e55c4d168e1df93c3700
Istam Rakhimov <vupmud@gmail.com>
2024-05-02 13:47:42 +0300

    Updated MedCocoMetric

--- mmdet/evaluation/metrics/__init__.py
+++ mmdet/evaluation/metrics/__init__.py
@@ -3,6 +3,7 @@ from .base_video_metric import BaseVideoMetric
 from .cityscapes_metric import CityScapesMetric
 from .coco_caption_metric import COCOCaptionMetric
 from .coco_metric import CocoMetric
+from .med_coco_metric import MedCocoMetric
 from .coco_occluded_metric import CocoOccludedSeparatedMetric
 from .coco_panoptic_metric import CocoPanopticMetric
 from .coco_video_metric import CocoVideoMetric
@@ -26,7 +27,7 @@ from .youtube_vis_metric import YouTubeVISMetric
 
 __all__ = [
     'CityScapesMetric', 'CocoMetric', 'CocoPanopticMetric', 'OpenImagesMetric',
-    'VOCMetric', 'LVISMetric', 'CrowdHumanMetric', 'DumpProposals',
+    'VOCMetric', 'LVISMetric', 'MedCocoMetric', 'CrowdHumanMetric', 'DumpProposals',
     'CocoOccludedSeparatedMetric', 'DumpDetResults', 'BaseVideoMetric',
     'MOTChallengeMetric', 'CocoVideoMetric', 'ReIDMetrics', 'YouTubeVISMetric',
     'COCOCaptionMetric', 'SemSegMetric', 'RefSegMetric', 'RefExpMetric',
